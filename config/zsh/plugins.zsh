e69646343229522e4c327788b67a510fee9135d1
Kennan LeJeune <kennan@case.edu>
2020-01-27 03:04:30 -0500

    switch to zplug over antigen

--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -1,38 +1,35 @@
-source /usr/local/share/antigen/antigen.zsh
-# Load the oh-my-zsh's library.
-antigen use oh-my-zsh
+# Check if zplug is installed
+if [[ ! -d ~/.zplug ]]; then
+  git clone https://github.com/zplug/zplug ~/.zplug
+  source ~/.zplug/init.zsh && zplug update --self
+fi
 
-# Bundles from the default repo (robbyrussell's oh-my-zsh).
-antigen bundle git
-antigen bundle pip
-antigen bundle command-not-found
+# Essential
+source ~/.zplug/init.zsh
 
-# Syntax highlighting bundle.
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zsh-users/zsh-autosuggestions
+# Make sure to use double quotes to prevent shell expansion
+zplug "zsh-users/zsh-syntax-highlighting"
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/pip", from:oh-my-zsh
+zplug "plugins/command-not-foudn", from:oh-my-zsh
+zplug "plugins/sudo", from:oh-my-zsh
+zplug "plugins/common-aliases", from:oh-my-zsh
 
-# other packages
-antigen bundle sudo
-antigen bundle common-aliases
-# OS specific plugins
 if [[ $(uname -s) == 'Darwin' ]]; then
-    antigen bundle brew
-    antigen bundle brew-cask
-    antigen bundle gem
-    antigen bundle osx
-elif [[ $(uname -s) == 'Linux' ]]; then
-    # None so far...
+    zplug "plugins/brew", from:oh-my-zsh
+    zplug "plugins/osx", from:oh-my-zsh
+fi
 
-    if [[ $DISTRO == 'CentOS' ]]; then
-        antigen bundle centos
+zplug "themes/agnoster", from:oh-my-zsh
+
+# Install packages that have not been installed yet
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    else
+        echo
     fi
 fi
 
-# Load the theme.
-antigen theme agnoster
-
-# Tell Antigen that you're done.
-antigen apply
-autoload -Uz compinit
-compinit
-
+zplug load
