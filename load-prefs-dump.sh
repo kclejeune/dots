3ac95adc3e5d50bd73cd1c61dfc16bbf777b199b
Kennan LeJeune <kennan@case.edu>
2020-02-12 19:32:18 -0500

    add fish config, ssh symlink and update prefs

--- load-prefs-dump.sh
+++ load-prefs-dump.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 # NOTE: This won't work without importing private key
-# fingerprint 117A03CAE0A807366464C3EB767584B12DC335E2
-gpg --decrypt ./prefs.plist.gpg | defaults write
+# fingerprint 117A03CAE0A807366464C3EB767584B12DC335
+defaults write secrets/prefs.plist
